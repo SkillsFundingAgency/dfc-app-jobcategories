439185bdd9fdf17c6f8554050f55e53872afa7db
Mark Godfrey <thelious@gmail.com>
2020-01-14 09:40:58 +0000

    Updates initial layout

---
+++ Dfc.App.Categories.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Dfc.App.Categories.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
