41914d74be998850a2ff511d241056ebdf0d9351
Tom Tollafield <thomas.tollafield@gmail.com>
2020-05-12 15:26:21 +0100

    WIP - JP refresh

--- DFC.App.JobCategories.PageService/EventProcessorServices/EventProcessingService.cs
+++ DFC.App.JobCategories.PageService/EventProcessorServices/EventProcessingService.cs
@@ -1,4 +1,6 @@
-﻿using DFC.App.JobCategories.Data.Models;
+﻿using DFC.App.JobCategories.Data.Extensions;
+using DFC.App.JobCategories.Data.Models;
+using DFC.App.JobCategories.Data.Models.API;
 using DFC.App.JobCategories.PageService.Extensions;
 using Microsoft.Extensions.Logging;
 using System;
@@ -14,17 +16,59 @@ namespace DFC.App.JobCategories.PageService.EventProcessorServices
         private readonly ILogger<EventProcessingService> logger;
         private readonly IContentPageService<JobCategory> jobCategoryPageService;
         private readonly IContentPageService<JobProfile> jobProfilePageService;
+        private readonly IApiDataService<ServiceTaxonomyApiClientOptions> jobProfileApiService;
 
-        public EventProcessingService(ILogger<EventProcessingService> logger, IContentPageService<JobCategory> jobCategoryPageService, IContentPageService<JobProfile> jobProfilePageService)
+        public EventProcessingService(ILogger<EventProcessingService> logger, IContentPageService<JobCategory> jobCategoryPageService, IContentPageService<JobProfile> jobProfilePageService, IApiDataService<ServiceTaxonomyApiClientOptions> jobProfileApiService)
         {
             this.logger = logger;
             this.jobCategoryPageService = jobCategoryPageService;
             this.jobProfilePageService = jobProfilePageService;
+            this.jobProfileApiService = jobProfileApiService;
         }
 
-        public Task<HttpStatusCode> AddOrUpdateAsync(Uri url)
+        public async Task<HttpStatusCode> AddOrUpdateAsync(Uri url)
         {
-            throw new NotImplementedException();
+            var contentType = url.GetContentItemType().ToLower();
+            var id = url.GetContentItemId();
+            switch (contentType)
+            {
+                case "jobcategory":
+                    //Get JCs and associated JPS -> Reload
+                case "jobprofile":
+                    return await RefreshJobProfile(id).ConfigureAwait(false);
+                case "occupation":
+                    var jobProfilesWithOccupation = await GetJobProfilesByOccupationIdAsync(id).ConfigureAwait(false);
+                    var jobProfileUpdateTasks = jobProfilesWithOccupation.Select(x => RefreshJobProfile(x.DocumentId.Value));
+                    var results = await Task.WhenAll(jobProfileUpdateTasks).ConfigureAwait(false);
+                    //Check all succesful?
+                    break;
+                case "occupationlabel":
+                    var jobProfilesWithOccupationLabels = await GetJobProfilesByOccupationLabelIdAsync(id).ConfigureAwait(false);
+                    var jobProfileWithOccupationLabelsUpdateTasks = jobProfilesWithOccupationLabels.Select(x => RefreshJobProfile(x.DocumentId.Value));
+                    await Task.WhenAll(jobProfileWithOccupationLabelsUpdateTasks).ConfigureAwait(false);
+                    break;
+                default:
+                    throw new InvalidOperationException($"Received message to delete type: {contentType}, id:{id}. There is no implementation present to process this type.");
+            }
+        }
+
+        private async Task<HttpStatusCode> RefreshJobProfile(Guid id)
+        {
+            var newJobProfileApi = await jobProfileApiService.GetByIdAsync<JobProfileApiResponse>(nameof(JobProfile).ToLower(), id).ConfigureAwait(false);
+            var newJobProfile = newJobProfileApi.Map();
+            return await jobProfilePageService.UpsertAsync(newJobProfile).ConfigureAwait(false);
+        }
+
+        private async Task<IEnumerable<JobProfile?>> GetJobProfilesByOccupationIdAsync(Guid occupationId)
+        {
+            var jobProfilesWithOccupation = await jobProfilePageService.GetByQueryAsync(x => x.Occupation.Uri.ToString().Contains(occupationId.ToString())).ConfigureAwait(false);
+            return jobProfilesWithOccupation;
+        }
+
+        private async Task<IEnumerable<JobProfile?>> GetJobProfilesByOccupationLabelIdAsync(Guid occupationLabelId)
+        {
+            var jobProfilesWithOccupationLabels = await jobProfilePageService.GetByQueryAsync(x => x.Occupation.OccupationLabels.Any(x => x.Uri.ToString().Contains(occupationLabelId.ToString()))).ConfigureAwait(false);
+            return jobProfilesWithOccupationLabels;
         }
 
         public async Task<HttpStatusCode> DeleteAsync(Uri url)
@@ -39,8 +83,7 @@ namespace DFC.App.JobCategories.PageService.EventProcessorServices
                     return await jobProfilePageService.DeleteAsync(id).ConfigureAwait(false);
                 case "occupation":
                     //Should never happen due to relationship restrictions in OC
-                    var jobProfilesWithOccupation = await jobProfilePageService.GetByQueryAsync(x => x.Occupation.Uri.ToString().Contains(id.ToString())).ConfigureAwait(false);
-
+                    var jobProfilesWithOccupation = await GetJobProfilesByOccupationIdAsync(id).ConfigureAwait(false);
                     if (jobProfilesWithOccupation != null)
                     {
                         jobProfilesWithOccupation.Select(c =>
@@ -59,7 +102,7 @@ namespace DFC.App.JobCategories.PageService.EventProcessorServices
 
                     break;
                 case "occupationlabel":
-                    var jobProfilesWithOccupationLabels = await jobProfilePageService.GetByQueryAsync(x => x.Occupation.OccupationLabels.Any(x => x.Uri.ToString().Contains(id.ToString()))).ConfigureAwait(false);
+                    var jobProfilesWithOccupationLabels = await GetJobProfilesByOccupationLabelIdAsync(id).ConfigureAwait(false);
                     foreach (var jp in jobProfilesWithOccupationLabels)
                     {
                         var newOccLabels = jp!.Occupation!.OccupationLabels.Where(x => !x.Uri.ToString().Contains(id.ToString()));
